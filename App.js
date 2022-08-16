9066886fd177955c4d9a600508199b017bfecc84
edwardsuwirya <edward_suwirya@yahoo.com>
2022-08-16 15:56:22 +0700

    fix load custom fonts error

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import WelcomePage from "./src/features/Welcome/WelcomePage";
-import MainContainer from "./src/shared/components/MainContainer";
-import {StatusBar, Text} from "react-native";
 import LoginPage from "./src/features/Login/LoginPage";
 import {ThemeProvider} from "./src/shared/context/ThemeContext";
 import useAppFont from "./src/shared/hook/UseAppFont";
 
 export default function App() {
     const fonts = useAppFont();
+
+    if (!fonts) {
+        return null;
+    }
     return (
         <ThemeProvider>
             {/*<MainContainer>*/}
