c01c2d900bce9a35d647fc55542487e1875366f7
edwardsuwirya <edward_suwirya@yahoo.com>
2022-08-26 10:52:14 +0700

    Using Safe Area Context

--- src/features/Welcome/WelcomePage.js
+++ src/features/Welcome/WelcomePage.js
@@ -3,10 +3,13 @@ import LottieView from 'lottie-react-native';
 import FormButton from "../../shared/components/FormButton";
 import TitleLabel from "../../shared/components/TitleLabel";
 import AppBackground from "../../shared/components/AppBackground";
+import MainContainer from "../../shared/components/MainContainer";
+import {useTheme} from "../../shared/context/ThemeContext";
 
 const WelcomePage = () => {
+    const theme = useTheme()
     return (
-        <View style={styles.container}>
+        <MainContainer>
             <AppBackground style={{
                 justifyContent: "space-evenly",
                 alignItems: 'center'
@@ -17,21 +20,17 @@ const WelcomePage = () => {
                     source={require('../../../assets/img/67221-food-market-app-interaction.json')}
                 />
                 <View style={styles.titleContainer}>
-                    <TitleLabel text='POS System'/>
-                    <TitleLabel subTitle text='Simple Point Of Sales'/>
+                    <TitleLabel text='POS System' titleStyle={theme.text.title}/>
+                    <TitleLabel subTitle text='Simple Point Of Sales' titleStyle={theme.text.subtitle}/>
                 </View>
                 <FormButton label='Sign In' onClick={() => {
                 }}></FormButton>
             </AppBackground>
-        </View>
+        </MainContainer>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "stretch",
-    },
     image: {
         width: 200,
         height: 200,
